87a87b674d91a57d6fc557c43b3212ef9825f1b8
Sasijaya2112 <Sasijaya2112@gmail.com>
2023-12-19 15:18:22 +0530

    new images added to gallery

--- src/Home/Slide1.js
+++ src/Home/Slide1.js
@@ -2,6 +2,9 @@ import Carousel from 'react-bootstrap/Carousel';
 import slide1 from './Slide Images/Kamalsir.jpg'
 import slide2 from './Slide Images/schoolclass.jpg'
 import slide3 from './Slide Images/smvec.jpg'
+import slide4 from './Slide Images/agridrone.jpg';
+import slide5 from './Slide Images/kamal.PNG';
+import slide6 from './Slide Images/petit.jpg';
 
 function Slide1() {
   return (
@@ -26,6 +29,27 @@ function Slide1() {
       <img
           className="d-block w-100"
           src={slide3}
+          alt="First slide"
+        /> 
+        </Carousel.Item>
+        <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src={slide4}
+          alt="First slide"
+        /> 
+        </Carousel.Item>
+        <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src={slide5}
+          alt="First slide"
+        /> 
+        </Carousel.Item>
+        <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src={slide6}
           alt="First slide"
         /> 
         </Carousel.Item>
--- src/Home/Slide2.js
+++ src/Home/Slide2.js
@@ -2,6 +2,9 @@ import Carousel from 'react-bootstrap/Carousel';
 import slide1 from './Slide Images/agri.jpg'
 import slide2 from './Slide Images/church.jpg'
 import slide3 from './Slide Images/harbour.jpg'
+import slide4 from './Slide Images/agridrone.jpg'
+import slide5 from './Slide Images/aerialdji.JPG'
+import slide6 from './Slide Images/pondy.JPG'
 
 function Slide2() {
   return (
@@ -26,6 +29,27 @@ function Slide2() {
       <img
           className="d-block w-100"
           src={slide3}
+          alt="First slide"
+        /> 
+        </Carousel.Item>
+        <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src={slide4}
+          alt="First slide"
+        /> 
+        </Carousel.Item>
+        <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src={slide5}
+          alt="First slide"
+        /> 
+        </Carousel.Item>
+        <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src={slide6}
           alt="First slide"
         /> 
         </Carousel.Item>
